544f8daabaf4159c9712db6c8077ab49861f6b37
bodzan <bodzan@tlen.pl>
2016-11-30 20:13:41 +0100

    zad5

--- funkcje/zad5.js
+++ funkcje/zad5.js
@@ -3,23 +3,24 @@ Stwórz funkcje zachowujące się tak samo jak:
  - Array.prototype.filter,
  - Array.prototype.map*/
 
- var table = ['a',5,1,7,5,'a',1,4,5,7,5,'b',5,'a',1];
+ var table = [5,1,7,5];
  var out =[];
 
-function notNumber(table){
-    for(var i=0;i<table.length;i++){
-        if(typeof(table[i])==='string'){
-            out.push(table[i]);
+function mnozenie(index) {
+    return index=index*3;
+}
+
+Array.prototype.myMap = 
+     function(funkcja){
+        var tab=[];
+        i=0;
+        for(var value of this){
+            tab[i]=funkcja(value);
+            i++;
+        }
+        for(var value of tab){
+            console.log(value);
         }
     }
-    return out;
-}
 
-function tableOut(out){
-    for(var i=0;i<out.length;i++){
-        console.log(out[i]);
-    }
-}
-
-out=notNumber(table);
-tableOut(out);
+out=table.myMap(mnozenie);
