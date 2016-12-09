692d98984061dfa214396bbd239152e02c15d76e
bodzan <bodzan@tlen.pl>
2016-12-09 15:46:26 +0100

    zad8

---
+++ funkcje/zad8.js
@@ -0,0 +1,33 @@
+// Zadanie 8.
+
+// Napisz funkcję sum4, taką, że:
+
+// Funkcja sum powinna zsumować cztery elementy, natomiast można ją wywołać na parę sposobów, tak, żeby łącznie przekazać 4 argumenty:
+
+// sum4(1, 2, 3, 4) // 10
+// sum4(1)(2)(3)(4) // 10
+// sum4(1, 2)(3, 4) // 10 
+// itd.
+
+function sum4() {
+    var args = 4;
+    var sum = 0;
+
+    function sumInside() {
+        for (let value of arguments) {
+            sum = sum + value;
+            args -= 1;
+            if (args === 0) return sum
+        }
+        return sumInside;
+    }
+    for (let value of arguments) {
+        sum = sum + value;
+        args -= 1;
+        if (args === 0) return sum
+    }
+    sumInside();
+    return sumInside;
+}
+
+console.log('Wynik dodawania czterech liczb : ' + sum4(1)(2)(3)(4, 5));
