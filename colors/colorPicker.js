d3d49bd7624c483f2f5984ff95b8726772ed87bb
bodzan <bodzan@tlen.pl>
2017-02-07 00:06:15 +0100

    colorPicker

--- colors/colorPicker.js
+++ colors/colorPicker.js
@@ -7,76 +7,83 @@ window.addEventListener('load', () => {
 
 class ColorPicker {
     constructor() {
-        this.colorPicker = document.getElementById('colorPicker');
-        this.colorSample = document.getElementById('colorSample');
-        this.rgbValue = document.getElementById('rgbValue');
-        this.hexValue = document.getElementById('hexValue');
-        this.hslValue = document.getElementById('hslValue');
-        this.numberValue = document.getElementById('numberValue');
-        this.rValue = document.getElementById('rValue'); //na pasku zrobić kolory
+        this.getDOMvalues();
+        this.initializeElements();
         this.addListeners();
-        this.colorPickerInitialize()
     }
 
-    addListeners() {
-        this.rValue.addEventListener("change", function() { this.rValueChange(this.rValue.value); }.bind(this));
-        this.colorPicker.addEventListener('click', function(e) { this.clickedColor(e); }.bind(this));
+    getDOMvalues() {
+        this.colorPalette = document.getElementById('colorPalette');
+        this.colorPrewiew = document.getElementById('colorPrewiew');
+        this.rgbText = document.getElementById('rgbText');
+        this.hexText = document.getElementById('hexText');
+        this.hslText = document.getElementById('hslText');
+        this.numberText = document.getElementById('numberText');
+        this.colorChanger = document.getElementById('colorChanger');
     }
-
-    colorPickerInitialize() {
-        this.colorPicker.width = 255;
-        this.colorPicker.height = 255;
-        this.context = this.colorPicker.getContext('2d');
-        this.drawColorPicker(0);
-        this.drawCircle(130, 130, "rgb(0,130,130)");
+    initializeElements() {
+        this.ColorSamplePositionX = 130;
+        this.ColorSamplePositionY = 130;
+        this.gFromRgb = 130;
+        this.bFromRgb = 130;
+        this.colorPalette.width = 255;
+        this.colorPalette.height = 255;
+        this.contextColorPalette = this.colorPalette.getContext('2d');
+        this.colorPrewiew.style.background = "rgb(0,130,130)";
+        this.hexText.value = "hex(#008282)";
+        this.hslText.value = "hsl(180,100%,25%)";
+        this.numberText.value = "number(33410)";
+        this.drawColorPalette(0);
+        this.drawColorSample("rgb(0,130,130)");
     }
-
-    drawColorPicker(r) {
+    drawColorPalette(r) {
         let rgb, g, b;
         for (g = 0; g < 256; g += 1) {
             for (b = 0; b < 256; b += 1) {
                 rgb = "rgb(" + r + "," + g + "," + b + ")";
-                this.context.fillStyle = rgb;
-                this.context.fillRect(b, g, 1, 1);
+                this.contextColorPalette.fillStyle = rgb;
+                this.contextColorPalette.fillRect(b, g, 1, 1);
             }
         }
     }
-
+    drawColorSample(rgb) {
+        this.contextColorPalette.beginPath();
+        this.contextColorPalette.arc(this.ColorSamplePositionX, this.ColorSamplePositionY, 8, 0, 10 * Math.PI);
+        this.contextColorPalette.strokeStyle = "white";
+        this.contextColorPalette.fillStyle = rgb;
+        this.contextColorPalette.lineWidth = 6;
+        this.contextColorPalette.stroke();
+        //this.contextColorPalette.closePath;
+        this.contextColorPalette.fill();
+    }
+    addListeners() {
+        this.colorChanger.addEventListener("change", function() { this.runColorChanger(parseInt(this.colorChanger.value)); }.bind(this));
+        this.colorPalette.addEventListener('click', function(e) { this.clickedColor(e); }.bind(this));
+    }
     clickedColor(e) {
-        let r = this.context.getImageData(e.offsetX, e.offsetY, 1, 1).data[0];
-        this.g = this.context.getImageData(e.offsetX, e.offsetY, 1, 1).data[1];
-        this.b = this.context.getImageData(e.offsetX, e.offsetY, 1, 1).data[2];
-        this.color = new Color(r, this.g, this.b);
-        this.colorSample.style.background = this.color.toRgb();
-        this.rgbValue.textContent = this.color.toRgb();
-        this.hexValue.textContent = this.color.toHex();
-        this.hslValue.textContent = this.color.toHsl();
-        this.numberValue.textContent = this.color.toNumber();
-        this.drawColorPicker(r);
-        this.drawCircle(e.offsetX, e.offsetY, this.color.toRgb());
+        let rFromRgb = this.contextColorPalette.getImageData(e.offsetX, e.offsetY, 1, 1).data[0];
+        this.gFromRgb = this.contextColorPalette.getImageData(e.offsetX, e.offsetY, 1, 1).data[1];
+        this.bFromRgb = this.contextColorPalette.getImageData(e.offsetX, e.offsetY, 1, 1).data[2];
+        this.ColorSamplePositionX = e.offsetX;
+        this.ColorSamplePositionY = e.offsetY;
+        this.color = new Color(rFromRgb, this.gFromRgb, this.bFromRgb);
+        this.colorPrewiew.style.background = this.color.toRgb();
+        this.rgbText.value = this.color.toRgb();
+        this.hexText.value = this.color.toHex();
+        this.hslText.value = this.color.toHsl();
+        this.numberText.value = this.color.toNumber();
+        this.drawColorPalette(rFromRgb);
+        this.drawColorSample(this.color.toRgb());
+    }
+    runColorChanger(r) {
+        this.drawColorPalette(r);
+        this.color = new Color(r, this.gFromRgb, this.bFromRgb);
+        this.colorPrewiew.style.background = this.color.toRgb();
+        this.rgbText.value = this.color.toRgb();
+        this.hexText.value = this.color.toHex();
+        this.hslText.value = this.color.toHsl();
+        this.numberText.value = this.color.toNumber();
+        this.drawColorSample(this.color.toRgb());
     }
 
-    drawCircle(x, y, rgb) {
-        this.x = x;
-        this.y = y;
-        this.context.beginPath();
-        this.context.arc(this.x, this.y, 8, 0, 10 * Math.PI);
-        this.context.strokeStyle = "white";
-        this.context.fillStyle = rgb;
-        this.context.lineWidth = 6;
-        this.context.stroke();
-        //this.context.closePath;
-        this.context.fill();
-    }
-    rValueChange(r) {
-            this.drawColorPicker(r);
-            this.color = new Color(r, this.g, this.b);
-            this.colorSample.style.background = this.color.toRgb();
-            this.rgbValue.textContent = this.color.toRgb();
-            this.hexValue.textContent = this.color.toHex();
-            this.hslValue.textContent = this.color.toHsl();
-            this.numberValue.textContent = this.color.toNumber();
-            this.drawCircle(this.x, this.y, this.color.toRgb());
-        }
-        //zrobić ładnie stronę z wpisem do formularza
 }
